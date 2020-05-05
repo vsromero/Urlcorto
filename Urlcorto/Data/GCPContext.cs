a8a386abc60beee5b0fcf44289166890be9421a8
Victor Sandoval Romero <36465564+vsromero@users.noreply.github.com>
2020-05-04 18:29:46 -0700

    Add GCPContext class

---
+++ Urlcorto/Data/GCPContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Urlcorto.Data
+{
+    public class GCPContext : DbContext
+    {
+        public GCPContext(DbContextOptions<GCPContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Urlcorto.Models.Url> Urls { get; set; }
+    }
+}
